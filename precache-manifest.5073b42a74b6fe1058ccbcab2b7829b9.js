f9c15fec847984a7e28d9edb7ff1be94b6917012
ToppleTheNun <ToppleTheNun@users.noreply.github.com>
2020-01-19 21:13:52 +0000

    Automated deployment: Sun Jan 19 21:13:52 UTC 2020 512f842efdfee54f3b9c759ed7ecad516f945fbd

--- precache-manifest.5073b42a74b6fe1058ccbcab2b7829b9.js
+++ precache-manifest.5073b42a74b6fe1058ccbcab2b7829b9.js
@@ -1,23 +1,23 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "80ae8561b2eb4d382c81e937a789d64c",
+    "revision": "6e7e27378d3a2c5a1dcf5bf327e784f5",
     "url": "/awetosis-topic-widget/index.html"
   },
   {
-    "revision": "88cbea79eaf4812b56a6",
+    "revision": "26830893ce042bde0c24",
     "url": "/awetosis-topic-widget/static/css/main.ee54585b.chunk.css"
   },
   {
-    "revision": "344a5e5f6f210e079160",
-    "url": "/awetosis-topic-widget/static/js/2.a61a2506.chunk.js"
+    "revision": "9edab04daf6a4a768ada",
+    "url": "/awetosis-topic-widget/static/js/2.f5d83159.chunk.js"
   },
   {
     "revision": "fd228f9b9a68ce6b23a6cf8b0a38a0e3",
-    "url": "/awetosis-topic-widget/static/js/2.a61a2506.chunk.js.LICENSE"
+    "url": "/awetosis-topic-widget/static/js/2.f5d83159.chunk.js.LICENSE"
   },
   {
-    "revision": "88cbea79eaf4812b56a6",
-    "url": "/awetosis-topic-widget/static/js/main.ba1c8253.chunk.js"
+    "revision": "26830893ce042bde0c24",
+    "url": "/awetosis-topic-widget/static/js/main.68583ffd.chunk.js"
   },
   {
     "revision": "c4a1dfa0e3508b0edcee",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/awetosis-topic-widget/precache-manifest.f70800b433866e798931a6074ee8a58c.js"
+  "/awetosis-topic-widget/precache-manifest.5073b42a74b6fe1058ccbcab2b7829b9.js"
 );
 
 self.addEventListener('message', (event) => {
