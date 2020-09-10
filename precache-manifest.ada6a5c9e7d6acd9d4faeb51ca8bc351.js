bc543a1f431c27764e1d78e72c866221be53fd1e
ToppleTheNun <ToppleTheNun@users.noreply.github.com>
2020-09-10 04:12:50 +0000

    Automated deployment: Thu Sep 10 04:12:50 UTC 2020 6f4eddb1700f948597fe2bed9b8e9fbcf93aeac9

---
+++ precache-manifest.ada6a5c9e7d6acd9d4faeb51ca8bc351.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc7e57a7c896d1b45fb13861a8100a13",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5deb5a97241312cfad62",
+    "url": "/static/css/main.ee54585b.chunk.css"
+  },
+  {
+    "revision": "4645ff646afcd22e89f4",
+    "url": "/static/js/2.3dcf39c0.chunk.js"
+  },
+  {
+    "revision": "c87550333da8df8ab4c80bd1d6e9defb",
+    "url": "/static/js/2.3dcf39c0.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "5deb5a97241312cfad62",
+    "url": "/static/js/main.9fe087cf.chunk.js"
+  },
+  {
+    "revision": "b11facd18a398797a352",
+    "url": "/static/js/runtime-main.f8dbcd91.js"
+  },
+  {
+    "revision": "4899ed6ef5cc72d274978a3816853769",
+    "url": "/static/media/orc_face.4899ed6e.png"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/awetosis-topic-widget/precache-manifest.095f300affdd787face032168996a738.js"
+  "/precache-manifest.ada6a5c9e7d6acd9d4faeb51ca8bc351.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/awetosis-topic-widget/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
